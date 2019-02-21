e42f57ace08e5e13b283e89da8bf1970bf4703f0
Ravi Jasti <jasti.mba@gmail.com>
2019-02-21 01:46:09 -0500

    Adjust buildpacks on Heroku

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -24,7 +24,7 @@ SECRET_KEY = '@!-)jwmuzh8btr380g61=g+#&zzei&dz2(&=xbvxztady)_p(r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'vriksh.herokuapp.com']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ravi.algarytm@gmail.com' # sendgrid
